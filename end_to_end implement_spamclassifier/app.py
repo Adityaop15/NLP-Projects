915abaafe4c3a881d6b941a428f9fff162460de9
Adityaop15 <114340319+Adityaop15@users.noreply.github.com>
2022-09-25 23:10:43 +0530

    Add files via upload

---
+++ end_to_end implement_spamclassifier/app.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 21:33:31 2022
+
+@author: Home
+"""
+
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+#from sklearn.externals import joblib
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	df= pd.read_csv("D:\\NLP_TEST\\spam.csv", encoding="latin-1")
+	
+	# Features and Labels
+	df['label'] = df['v1'].map({'Not Spam': 0, 'spam': 1})
+	X = df['v2']
+	y = df['label']
+	
+	# Extract Feature With CountVectorizer
+	cv = CountVectorizer()
+	X = cv.fit_transform(X) # Fit the Data
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	#Naive Bayes Classifier
+	from sklearn.naive_bayes import MultinomialNB
+
+	clf = MultinomialNB()
+	clf.fit(X_train,y_train)
+	clf.score(X_test,y_test)
+	
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=False)
